6c2666c0c8b246327c163667353d307d98053569
mbenitez <mbenitez.a@gmail.com>
2023-02-23 14:54:59 -0500

    Add resume page

---
+++ src/components/pages/Resume.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export default function Resume() {
+  return (
+    <div className="vh-75">
+      <div className="container w-75">
+        <div className="card bg-light mb-3">
+          <div className="card-header">Experience</div>
+          <div className="card-body">
+            <ul>
+              <li>Full Stack web development</li>
+              <ul>
+                <li>HTML, CSS (Bootstrap)</li>
+                <li>JavaScript</li>
+                <li>Node.js</li>
+                <li>Express</li>
+                <li>React</li>
+                <li>MERN</li>
+                <li>Databases</li>
+                <ul>
+                  <li>MySQL</li>
+                  <li>NoSQL</li>
+                </ul>
+                <li>APIs</li>
+                <ul>
+                  <li>Web</li>
+                  <li>Third Party</li>
+                  <li>Server Side</li>
+                </ul>
+              </ul>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
